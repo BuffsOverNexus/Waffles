99f78d20de9b67a7ff4fb70da6ddcc8de854b086
Jared Bates <computerwizjared@hotmail.com>
2014-11-18 17:51:35 -0600

    Working on it....

--- index.php
+++ index.php
@@ -4,9 +4,16 @@
 <html>
 <head lang="en">
     <meta charset="UTF-8">
+
+    <link rel="stylesheet" href="dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="dist/css/style.css">
+
     <title></title>
 </head>
 <body>
+    <?php include 'includes/header.phtml';?>
 
+    <script src="dist/js/jquery.min.js"></script>
+    <script src="dist/js/bootstrap.min.js"></script>
 </body>
 </html>
