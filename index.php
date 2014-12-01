0807dce6b177703d3cba207a7c64a11c69c4c1b9
Jared Bates <computerwizjared@hotmail.com>
2014-12-01 17:55:29 -0600

    Update IP

--- index.php
+++ index.php
@@ -2,13 +2,14 @@
 <!DOCTYPE html>
 <?php
 // Edit this ->
-define('MQ_SERVER_ADDR', '198.100.146.49');
+define('MQ_SERVER_ADDR', 'breakfastcraft.rocks');
 define('MQ_SERVER_PORT', 25565);
 define('MQ_TIMEOUT', 1);
 // Edit this <-
 $host = "127.0.0.1";
 $username = "bukkit";
 $password = "ufWjP7SPLU7DwQj3";
+$online = false;
 
 $connection = mysqli_connect($host, $username, $password) or die("Unable to find database.");
 //$selectDB = @mysql_select_db($username, $connection) or die("Error: Unable to Find Database");
@@ -30,8 +31,9 @@ try {
     $Exception = $e;
 }
 
-if (($playersArray = $query->GetPlayers()) !== false) {
+if (($playersArray = $query->GetPlayers()) == true) {
     $players = sizeof($playersArray);
+    $online = true;
 } else {
     $players = 0;
 }
@@ -65,7 +67,8 @@ if (($playersArray = $query->GetPlayers()) !== false) {
         <div class="col-lg-4 col-md-offset-4">
             <div class="panel panel-default">
                 <div class="panel-body">
-                    <span class="label label-default center-block"><h5>JOIN NOW: <b><?php echo MQ_SERVER_ADDR ?></b></h5></span>
+                    <span class="label label-default center-block"><h5>JOIN NOW: <b><?php echo MQ_SERVER_ADDR; ?></b></h5></span>
+                    <?php if ($online): ?>
                     <span class="label label-success center-block"><h5>Currently Online:</h5></span>
                     <div class="progress">
                         <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: <?php echo ($players / 60) * 100; ?>%">
@@ -75,7 +78,11 @@ if (($playersArray = $query->GetPlayers()) !== false) {
                     <?php if ($players > 0) :
                     foreach ((array) $playersArray as $player): ?>
                         <img src="https://minotar.net/avatar/<?php echo $player; ?>/32">
-                    <?php endforeach; endif; ?>
+                    <?php endforeach;
+                        endif;
+                    else: ?>
+                        <span class="label label-danger center-block"><h5><b>OFFLINE</b></h5></span>
+                    <?php endif; ?>
                 </div>
             </div>
         </div>
